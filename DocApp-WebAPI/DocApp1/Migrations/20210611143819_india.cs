346f4f7e9c351afc51450ad319ed17e67ca3c8ee
Munsif Khan <munsifkhan.i@sonata-software.com>
2021-06-11 20:34:29 +0530

    initial1

---
+++ DocApp-WebAPI/DocApp1/Migrations/20210611143819_india.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DocApp1.Migrations
+{
+    public partial class india : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
